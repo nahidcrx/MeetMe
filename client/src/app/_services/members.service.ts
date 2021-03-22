9b54716373174e50e9bb91aa541d8d2c7da3d66e
naymul.islam <naymul.islam@summitcommunications.net>
2021-03-22 13:48:54 +0600

    User interface built

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,14 +1,7 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
-
-const httpOptions = {
-  headers: new HttpHeaders({
-    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user'))?.token
-  })
-}
 
 @Injectable({
   providedIn: 'root'
@@ -22,9 +15,9 @@ export class MembersService {
   }
 
   getMembers(){
-    return this.http.get<Member[]>(this.baseUrl + "users", httpOptions);
+    return this.http.get<Member[]>(this.baseUrl + "users");
   }
   getMember(userName: string){
-    return this.http.get<Member>(this.baseUrl + "users" + userName, httpOptions);
+    return this.http.get<Member>(this.baseUrl + "users/" + userName);
   }
 }
