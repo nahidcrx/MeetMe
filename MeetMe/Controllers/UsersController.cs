e1802b02026f82c8d1e98859cf7b457c20b2ee58
naymul.islam <naymul.islam@summitcommunications.net>
2021-03-25 14:07:10 +0600

    Update loggedin user profile and added spinner

--- MeetMe/Controllers/UsersController.cs
+++ MeetMe/Controllers/UsersController.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace MeetMe.Controllers
@@ -39,6 +40,19 @@ namespace MeetMe.Controllers
             return await _userRepository.GetMemberByUserNameAsync(username);
         }
 
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto) 
+        {
+            var userName = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUserNameAsync(userName);
+
+            _mapper.Map(memberUpdateDto, user);
+            _userRepository.Update(user);
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
+        }
         //[HttpGet("{id}")]
         //public async Task<ActionResult<MemberDto>> GetUser(int id)
         //{
